916ca74ccf78f227f162ce5e0d7e0a8e7918d9b2
PanichMarkVladislavovich <panich.m.v@gmail.com>
2025-10-23 17:22:46 +0300

    Added basic PlayerHealth logic

---
+++ Assets/HUD/PlayerHealthManager.cs
@@ -0,0 +1,75 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthManager : MonoBehaviour
+{
+	public static PlayerHealthManager Instance { get; private set; } // Статическое поле экземпляра
+
+	public Slider HealthBarSlider;
+    public Button HealingItemButton;
+    public TextMeshProUGUI HealingItemNumber;
+    public int MaxPlayerHealth { get; private set; } = 100;
+    public int CurrentPlayerHealth { get; private set; } = 30;
+
+    public int MaxHealingItemsNumber { get; private set; } = 9;
+
+	public int CurrentHealingItemsNumber { get; private set; } = 5;
+
+	private void Awake()
+	{
+		// Паттерн Singleton: предотвращаем создание второго экземпляра
+		if (Instance == null)
+		{
+			Instance = this;
+			DontDestroyOnLoad(gameObject); // Сохраняется при смене уровней
+		}
+		else
+		{
+			Destroy(gameObject); // Уничтожаем лишние экземпляры
+		}
+	}
+
+	void Start()
+    {
+        HealingItemButton.onClick.AddListener(() => UseHealingItem());
+
+        HealthBarSlider.maxValue = MaxPlayerHealth;
+
+    }
+
+    void Update()
+    {
+        HealthBarSlider.value = CurrentPlayerHealth;
+
+        HealingItemNumber.text = CurrentHealingItemsNumber.ToString();
+	}
+
+    private void UseHealingItem()
+    {
+        if (CurrentHealingItemsNumber > 0)
+        {
+            if (CurrentPlayerHealth < MaxPlayerHealth)
+            {
+            Debug.Log("Used Healing Item");
+            CurrentHealingItemsNumber--;
+
+                CurrentPlayerHealth += 34;
+            }
+            else Debug.Log("Health is already Full");
+		}
+		else Debug.Log("0 Healing Items");
+
+	}
+    public void AddHealingItem()
+    {
+		if (CurrentHealingItemsNumber < 9)
+        {
+			Debug.Log("Added Healing Item");
+			CurrentHealingItemsNumber++;
+		}
+        else Debug.Log("Max Healing Items");
+
+	}
+
+}
---
+++ Assets/Interaction/Loot Items/LootItemHealingItem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LootItemHealingItem : LootItemAbstract
+{
+
+	public override int MoneyValue { get; protected set; } = 0;
+
+	public override string ItemName => "Лечащий предмет";
+
+
+	public override void Interact()
+	{
+		if (PlayerHealthManager.Instance.CurrentHealingItemsNumber < 9)
+		{
+			Debug.Log($"Вы подняли {ItemName}");
+			Destroy(gameObject);
+			PlayerHealthManager.Instance.AddHealingItem();
+		}
+		else Debug.Log("Can't pick up more Healing Items");
+
+	}
+}
