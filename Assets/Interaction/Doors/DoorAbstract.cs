cca5d4cb226fc5af73a42b5618fbf046f8892f28
PanichMarkVladislavovich <panich.m.v@gmail.com>
2025-10-29 15:29:45 +0300

    Added interactable Door

---
+++ Assets/Interaction/Doors/DoorAbstract.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System;
+using Unity.IO.LowLevel.Unsafe;
+
+public abstract class DoorAbstract : MonoBehaviour, IInteractable
+{
+	//public virtual Vector3 LootItemPosition { get; protected set; }
+	//public virtual Quaternion LootItemRotation { get; protected set; }
+	public virtual string ItemName => gameObject.name;
+
+	public virtual string InteractionHint => $"Открыть {ItemName}";
+
+
+	public virtual bool IsDoorOpened { get; protected set; }
+
+
+	public int DoorIndex { get; protected set; }
+	
+	/*
+	internal void AssignLootItemIndex(int index)
+	{
+		Door = index;
+	}
+*/
+
+	public abstract void Interact();
+
+
+	/*
+	public abstract void LoadData(GameData data);
+
+
+	public abstract void SaveData(ref GameData data);
+	*/
+
+}
---
+++ Assets/Interaction/Doors/DoorSingleDoor.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections;
+
+public class DoorSingleDoor : DoorAbstract
+{
+	public override string ItemName => "Дверь";
+
+	private float doorOpeningSpeed = 200f; // Скорость открытия-закрытия
+
+	private Coroutine currentAnimation;     // Переменная для хранения активной корутины
+
+	private Quaternion openedRotation;       // Угловое положение открытой двери
+	private Quaternion closedRotation;     // Угловое положение закрытой двери
+
+	void Start()
+	{
+		// Настройка состояний вращения
+		Vector3 openedEulerAngles = new Vector3(0, 0, 90);
+		openedRotation = Quaternion.Euler(openedEulerAngles);
+
+		Vector3 closedEulerAngles = new Vector3(0, 0, 0);
+		closedRotation = Quaternion.Euler(closedEulerAngles);
+
+		IsDoorOpened = false;
+	}
+
+	public override void Interact()
+	{
+		// Останавливаем ранее запущенную корутину, если она существует
+		if (currentAnimation != null)
+		{
+			StopCoroutine(currentAnimation);
+		}
+
+		if (!IsDoorOpened)
+		{
+			currentAnimation = StartCoroutine(OpenDoor()); // Начинаем новую корутину
+		}
+		else
+		{
+			currentAnimation = StartCoroutine(CloseDoor()); // Начинаем новую корутину
+		}
+	}
+
+	private void Update()
+	{
+		//Debug.Log(currentAnimation);
+	}
+
+	IEnumerator OpenDoor()
+	{
+		Debug.Log($"Вы открываете {ItemName}");
+		IsDoorOpened = true;
+
+		while (Quaternion.Angle(transform.localRotation, openedRotation) > 0.1f)
+		{
+			transform.localRotation = Quaternion.RotateTowards(transform.localRotation, openedRotation, Time.deltaTime * doorOpeningSpeed);
+			yield return null;
+		}
+
+		currentAnimation = null;
+	}
+
+	IEnumerator CloseDoor()
+	{
+		Debug.Log($"Вы закрываете {ItemName}");
+		IsDoorOpened = false;
+
+		while (Quaternion.Angle(transform.localRotation, closedRotation) > 0.1f)
+		{
+			transform.localRotation = Quaternion.RotateTowards(transform.localRotation, closedRotation, Time.deltaTime * doorOpeningSpeed);
+			yield return null;
+		}
+
+		currentAnimation = null;
+	}
+}
